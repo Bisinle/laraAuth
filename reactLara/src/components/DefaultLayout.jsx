3370dc9bb596739a86ef5b21f7a12cd15718c2a2
Bisinle <abdiwadud.mohamedd@gmail.com>
2024-09-05 00:45:24 +0300

    logout functionality works fine

--- reactLara/src/components/DefaultLayout.jsx
+++ reactLara/src/components/DefaultLayout.jsx
@@ -1,17 +1,29 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Link, Navigate, Outlet } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider";
+import axiosClient from "../axiosClient";
 
 export default function DefaultLayout() {
-    const { user, token } = useStateContext();
+    const { user, token, setUser, setToken, notification } = useStateContext();
+
     if (!token) {
         return <Navigate to="/login" />;
     }
 
     const onLogout = (ev) => {
         ev.preventDefault();
-        localStorage.removeItem("ACCESS_TOKEN");
+
+        axiosClient.post("/logout").then(() => {
+            setUser({});
+            setToken(null);
+        });
     };
+
+    useEffect(() => {
+        axiosClient.get("/user").then(({ data }) => {
+            setUser(data);
+        });
+    }, []);
     return (
         <div id="defaultLayout">
             <aside>

e4dc1d82fd6166876150701c03dc0d3d6028345a
Bisinle <abdiwadud.mohamedd@gmail.com>
2024-09-12 02:29:06 +0300

    added an image field to Use Model

---
+++ app/Filament/Pages/Dashboard.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ 
+ namespace App\Filament\Pages;
+ 
+ use Filament\Pages\Dashboard as BaseDashboard;
+ use Filament\Widgets\StatsOverviewWidget\Card;
+ use Filament\Widgets\AccountWidget;
+ use Filament\Widgets\FilamentInfoWidget;
+ use App\Filament\Widgets\StatsOverview;
+ use App\Filament\Widgets\LatestOrders;
+ use App\Filament\Widgets\BlogPostsChart;
+ use App\Models\Category;
+ use App\Models\Post;
+ use App\Models\User;
+ use Filament\Widgets\StatsOverviewWidget;
+ 
+ class Dashboard extends BaseDashboard
+ {
+     protected static ?string $navigationIcon = 'heroicon-o-home';
+     protected static string $view = 'filament.pages.dashboard';
+ 
+     protected function getWidgets(): array
+     {
+         return [
+             AccountWidget::class,
+             StatsOverviewWidget::class,
+             Post::class,
+             User::class,
+             Category::class,
+         ];
+     }
+ 
+     protected function getColumns(): int | array
+     {
+         return 3;
+     }
+ }
+
+ */

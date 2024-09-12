7d60a030bf8e0a580041b5da9379ff60f32642c2
Bisinle <abdiwadud.mohamedd@gmail.com>
2024-09-12 18:10:31 +0300

    created the pivot table post_tag

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -18,6 +18,10 @@ class Post extends Model
         return $this->belongsTo(User::class);
     }
 
+    // public function tags(){
+    //     return $this->belongsToMany(Tag::class, 'post_tag');
+    // }
+
     protected $fillable = [
         "title",
         "description",
